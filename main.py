d4ddc754cdd0ce375b294863779de9800a2e347d
Ivan Stoyanchev <104705438+vankata-129@users.noreply.github.com>
2023-08-01 00:37:55 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+import os
+import requests
+import json
+import openai
+import datetime
+
+# Get current date and time
+now = datetime.datetime.now()
+timestamp = now.strftime('%Y%m%d_%H%M')
+
+#To get the name of the file for the conversation
+filename = f'conversation_{timestamp}.txt'
+
+def call_gpt(prompt, role):
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": role, "content": f"{prompt}"}
+        ]
+    )
+    return response.choices[0].message['content'].strip()
+
+# Load the state machine from a file
+with open("state_machine.json") as file:
+    state_machine = json.load(file)
+
+# Get the OpenAI API key
+with open('api_key.txt', 'r') as file:
+    api_key = file.read().strip()
+
+openai.api_key = api_key
+
+# Initialize the state
+state = "Initial"
+user_response = ""
+
+# Limit the number of interactions
+max_interactions = 20
+interaction_count = 0
+
+with open(filename, 'w') as file:
+    # Main loop
+    while interaction_count < max_interactions and state != "Desired":
+        # Fetch actions for the current state
+        actions = next(s['actions'] for s in state_machine['states'] if s['name'] == state)
+
+        # Generate actions using GPT-3
+        if len(user_response) != 0:
+            action_prompt = f"User's response: {user_response}. The current state is {state}. The suggested actions are {actions}."
+            action_response = call_gpt(action_prompt, "assistant")
+            file.write(f"Assistant's response: {action_response}\n")
+
+            user_prompt = f"The actions taken are {action_response}. What is the user's response?"
+            user_response = call_gpt(user_prompt, "user")
+            file.write(f"User's response: {user_response}\n")
+        else:
+            action_prompt = f"You are IT Help Desk. You are given state machine in a JSON format for problem of a user. {state_machine}. Current state is 'initial', you need to help them reach 'Desired' state. Tell them what to do step-by-step and expect progress from the user. Tell what next steps to take. Treat it like a conversation."
+            action_response = call_gpt(action_prompt, "assistant")
+            file.write(f"Assistant's response: {action_response}\n")
+
+            user_prompt = f"Act like a user that is experiencing IT issues. Your problem given like a state machine looks like this in JSON format: {state_machine}. I am your IT Help Desk."
+            user_response = call_gpt(user_prompt, "user")
+            file.write(f"User's response: {user_response}\n")
+
+        # Update the state based on the user's responses and the defined transitions
+        for action in actions:
+            if action['name'] in user_response:
+                state = action['nextState']
+                break
+        print(f"The current state is {state}")
+        interaction_count += 1
+
+    print(interaction_count)
