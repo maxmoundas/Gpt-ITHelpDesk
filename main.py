29fd0df2c8503bba4155a6cd14d2eb7b9ade626e
vankata-129 <ivanstoyanchev@abv.bg>
2023-08-07 19:01:26 -0500

    Fixed prompting and desired state

--- main.py
+++ main.py
@@ -50,31 +50,34 @@ conversation_history = []
 
 with open(filename, 'w') as file:
     # Main loop
-    while interaction_count < max_interactions and state != "Desired":
+    while interaction_count < max_interactions and user_response != "Y":
         # Fetch actions for the current state
         actions = next(s['actions'] for s in state_machine['states'] if s['name'] == state)
 
         # Generate actions using GPT-3
         if len(user_response) != 0:
-            action_prompt = f"You are the Help Desk, The user's response is : {user_response}. The current state is {state}. The suggested actions are {actions}."
+            action_prompt = f"The user's response is: {user_response}. The current state is {state}. The suggested actions are {actions}."
             conversation_history.append({"role": "assistant", "content": action_prompt})
             action_response = call_gpt(conversation_history)
             file.write(f"Assistant's response: {action_response}\n")
 
-            user_prompt = f"You are the user experiencing issues. The actions taken are {action_response}. What is the user's response?"
+            user_prompt = f"The actions taken are {action_response}. What is the user's response? If the use is satified enough with the result, respond with 'Y' and nothing else. "
             conversation_history.append({"role": "user", "content": user_prompt})
             user_response = call_gpt(conversation_history)
-            file.write(f"User's response: {user_response}\n")
+            file.write(f"*******************************************************"
+                       f"User's response: {user_response}\n")
         else:
-            action_prompt = f"You are IT Help Desk. You are given state machine in a JSON format for problem of a user. {state_machine}. Current state is 'initial', you need to help them reach 'Desired' state. Tell them what to do step-by-step and expect progress from the user. Tell what next steps to take. Treat it like a conversation."
+            action_prompt = f"You are IT Help Desk. You are given state machine in a JSON format for problem of a user. {state_machine}. Current state is 'initial', you need to help them reach 'Desired' state. Tell them what to do step-by-step and expect progress from the user. Describe your action plan in a chain-of-thought. Tell what next steps to take. Treat it like a conversation."
             conversation_history.append({"role": "assistant", "content": action_prompt})
             action_response = call_gpt(conversation_history)
             file.write(f"Assistant's response: {action_response}\n")
 
-            user_prompt = f"Act like a user that is experiencing IT issues. Your problem given like a state machine looks like this in JSON format: {state_machine}. I am your IT Help Desk."
+            user_prompt = f"Act like a user that is experiencing IT issues. Your problem given like a state machine looks like this in JSON format: {state_machine}. The initial state is 'initial' and I will help you ge tot 'desired'. Follow my orders step by step and report to me with the result."
             conversation_history.append({"role": "user", "content": user_prompt})
             user_response = call_gpt(conversation_history)
-            file.write(f"User's response: {user_response}\n")
+            file.write(f"*******************************************************\n"
+                       f"User's response: {user_response}\n")
+
 
         # Update the state based on the user's responses and the defined transitions
         for action in actions:
@@ -83,4 +86,5 @@ with open(filename, 'w') as file:
                 break
         print(f"The current state is {state}")
         interaction_count +=1
+    file.write(f"Number of steps taken:{interaction_count}")
     print(interaction_count)
